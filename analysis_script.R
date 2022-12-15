fb6339891f1ac5e4c210d7fe99cc7c5601c1f7f8
equanimity-23 <danielchewie@gmail.com>
2022-12-15 13:52:19 +1000

    script on t-test for qPCR data

---
+++ analysis_script.R
@@ -0,0 +1,22 @@
+# qPCR analysis script
+
+## load required packages
+library(dplyr)
+library(broom)
+
+## import data
+data <- read.csv("sampleAnalysis.csv")
+
+## Test for normal distribution using Shapiro-Wilk's test
+shapiro.test(data$MNE)
+
+### assume data is normally distributed if p > 0.05. 
+### normal distribution, use parametric tests
+### non-normal distribution, use non-parametric test
+
+## Paired t-test with Welch's correction
+res <- tidy(t.test(MNE ~ Group, data))
+
+
+## print t-test results
+write.csv(res, "sampleResults.csv")
